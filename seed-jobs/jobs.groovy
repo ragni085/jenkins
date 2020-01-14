6e88d6f3dbcf8030758d484c1c1876e6d602f36c
Ragni kumari <ragni085@gmail.com>
2020-01-14 17:58:58 +0530

    some change

--- seed-jobs/jobs.groovy
+++ seed-jobs/jobs.groovy
@@ -3,7 +3,7 @@ pipelineJob('sample-pipeline') {
         flowerinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
             'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
                 'userRemoteConfigs' {
-                     hudson.plugins.git.UserRemoteConfig' {
+                    'hudson.plugins.git.UserRemoteConfig' {
                         'url'('https://github.com/ragni085/jenkins.git')
                         'credentialId'('Git-User')
                     }
