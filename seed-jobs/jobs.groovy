444c4a0975df9e05f8e56be1fa708ca5b4ed9ce3
Ragni kumari <ragni085@gmail.com>
2020-01-14 17:56:19 +0530

    copying file of jobs/dd.groovy

--- seed-jobs/jobs.groovy
+++ seed-jobs/jobs.groovy
@@ -1,17 +1,16 @@
-pipelineJob('example') {
-    definition {
-        cps {
-            scm {
-                 git {
-                     remote {
-                        url('git@server:account/repo1.git')
-                    }
-                    extensions {
-                        cleanAfterCheckout()
-                        relativeTargetDirectory('repo1')
+pipelineJob('sample-pipeline') {
+    configure { flowdefinition ->
+        flowerinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                     hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/ragni085/jenkins.git')
+                        'credentialId'('Git-User')
                     }
                 }
             }
+            'scriptPath'('pipeline-jobs/sample.Jenkinsfile')
+            'lightweight'(true)
         }
     }
 }
