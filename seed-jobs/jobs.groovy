7dc0c3a5cdb22cb71f98d8295d4353485571c28f
Ragni kumari <ragni085@gmail.com>
2020-01-14 18:22:28 +0530

    Adding CI-PIPELINE

--- seed-jobs/jobs.groovy
+++ seed-jobs/jobs.groovy
@@ -19,3 +19,26 @@ pipelineJob('sample-pipeline') {
         }
     }
 }
+
+
+pipelineJob('CI-pipeline') {
+    configure { flowdefinition ->
+        flowerinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/ragni085/jenkins.git')
+                        'credentialId'('Git-User')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/master')
+                    }
+                }
+            }
+            'scriptPath'('pipeline-jobs/ci.Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
